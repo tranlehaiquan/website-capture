bf15892b1cbc6bdc915f0ce5c7ea059dee003c66
tranlehaiquan <tranlehaiquan@gmail.com>
2023-06-30 23:00:41 +0700

    re install

---
+++ stacks/MyStack.ts
@@ -0,0 +1,17 @@
+import { StackContext, Api } from "sst/constructs";
+
+export function API({ stack }: StackContext) {
+  const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+      },
+    },
+    routes: {
+      "GET /": "packages/functions/src/lambda.handler"
+    },
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+}
