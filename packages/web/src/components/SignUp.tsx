a86e5c25ef2783811dc66288de484cef4cba8a03
Quan Tran <tranlehaiquan@gmail.com>
2023-11-06 00:17:01 +0700

    update version

--- packages/web/src/components/SignUp.tsx
+++ packages/web/src/components/SignUp.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
   className?: string;
-};
-
-const SignUp: React.FC<Props> = (props) => {
-  return (
-    <div>
-      Sign Up
-    </div>
-  );
 }
+
+const SignUp: React.FC<Props> = () => {
+  return <div>Sign Up</div>;
+};
 
 export default SignUp;
