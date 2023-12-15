c6d250e65b9fc29dcded2e33fce1791c2f1f1391
Quan Tran <tranlehaiquan@gmail.com>
2023-12-16 01:29:24 +0700

    Complete script setup

--- readme.md
+++ readme.md
@@ -5,24 +5,7 @@
 ## Getting started
 
 ```bash
-# make dir layers/chromium
-mkdir -p layers/chromium
-
-# download release from @sparticuz/chromium then extract to layers/chromium
-# https://github.com/Sparticuz/chromium/releases/tag/v119.0.0
-# result will be layers/chromium/nodejs
-
-# pnpm install
-pnpm install
-
-# open new terminal then install chromium
-# will print chromium path, then update packages/core/src/puppeteerWorker.ts YOUR_LOCAL_CHROMIUM_PATH with it
-npx @puppeteer/browsers install chromium@latest --path /tmp/localChromium
-```
-
-```bash
-mkdir -p localChromium
-# copy chromium to localChromium
+pnpm install && NODE_ENV=development node ./setup.js
 ```
 
 Set sst secrets
--- setup.js
+++ setup.js
@@ -1,6 +1,12 @@
 import fs from "fs-extra";
+import unzipper from "unzipper";
 import { pipeline } from "node:stream/promises";
-import unzipper from 'unzipper';
+import { execSync } from "node:child_process";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // mkdir layers/chromium
 const PATH_LAYERS = "layers/chromium";
@@ -21,3 +27,16 @@ const chromiumLayerLink =
 
   await pipeline(response.body, unzipper.Extract({ path: PATH_LAYERS }));
 })();
+
+// Setup localChromium if is dev mode
+const IS_DEV = process.env.NODE_ENV === "development";
+// mkdir localChromium
+if (IS_DEV) {
+  const chromiumLocal = "localChromium";
+  fs.mkdirsSync(chromiumLocal);
+  const PATH_LOCAL_CHROMIUM = join(__dirname, chromiumLocal);
+
+  execSync(
+    `npx @puppeteer/browsers install chromium@latest --path ${PATH_LOCAL_CHROMIUM}`
+  );
+}
