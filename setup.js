4fc8afaa042551126bc9ccac434818d752d02f86
Quan Tran <tranlehaiquan@gmail.com>
2023-12-16 01:07:30 +0700

    Complete add script setup

--- setup.js
+++ setup.js
@@ -1,8 +1,6 @@
 import fs from "fs-extra";
-import { pipeline } from "stream";
-import { promisify } from "util";
-
-const streamPipeline = promisify(pipeline);
+import { pipeline } from "node:stream/promises";
+import unzipper from 'unzipper';
 
 // mkdir layers/chromium
 const PATH_LAYERS = "layers/chromium";
@@ -15,24 +13,11 @@ const chromiumLayerLink =
   "https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v119.0.0-layer.zip";
 
 (async () => {
-  const file = `${PATH_LAYERS}/chromium-v119.0.0-layer.zip`;
+  // const file = `${PATH_LAYERS}/chromium-v119.0.0-layer.zip`;
   const response = await fetch(chromiumLayerLink);
   if (!response.ok) {
     throw new Error(`Unexpected response ${response.statusText}`);
   }
 
-  await streamPipeline(response.body, fs.createWriteStream(file));
-  console.log("Download complete");
-
-  // const reader = chromiumLayerResponse.body.getReader();
-
-  // while (true) {
-  //   const { done, value } = await reader.read();
-  //   if (done) {
-  //     console.log("done");
-  //     break;
-  //   }
-
-  //   console.log(`Received ${value.length} bytes`);
-  // }
+  await pipeline(response.body, unzipper.Extract({ path: PATH_LAYERS }));
 })();
