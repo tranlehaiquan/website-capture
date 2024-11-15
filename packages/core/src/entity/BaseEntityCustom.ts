f0f0fe745f253df3ebeaa3383346df8bb8d961da
Q.Tran <quan.tran@metro.digital>
2024-11-15 18:48:20 +0700

    reset project

--- packages/core/src/entity/BaseEntityCustom.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, CreateDateColumn, UpdateDateColumn } from "typeorm";
-
-export class BaseEntityCustom extends BaseEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
--- packages/core/src/middlewares/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as baseMiddlewares } from "./baseMiddlewares";
-export { default as validator } from "./validator";
-export { default as connectDatabase } from "./connectDatabase";
